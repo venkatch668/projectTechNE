5bcba7f390f8ae0bbcc6536b7604f2887acd9de0
venkatababu <venkatababu.chekoddi@kanrad.com>
2022-12-29 21:17:27 +0530

    test

--- src/main/java/com/techne/demo/ProjectTechNe3Application.java
+++ src/main/java/com/techne/demo/ProjectTechNe3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectTechNeApplication {
+public class ProjectTechNe3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectTechNeApplication.class, args);
+		SpringApplication.run(ProjectTechNe3Application.class, args);
 	}
 
 }
--- src/test/java/com/techne/demo/ProjectTechNe3ApplicationTests.java
+++ src/test/java/com/techne/demo/ProjectTechNe3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectTechNeApplicationTests {
+class ProjectTechNe3ApplicationTests {
 
 	@Test
 	void contextLoads() {
